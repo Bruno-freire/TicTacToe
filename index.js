b5ba74b05279af979992e403da74cab50d8c8b63
Bruno Freire <brunofreire1717@gmail.com>
2022-12-14 14:11:04 -0300

    GameComplet

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ function disableRegion(element){
 }
 
 function initializeGame() {
+  if(document.getElementById('player1').value == "" || document.getElementById('player2').value == ""){
+    alert('Precisa haver dois jogadores')
+    return
+  }
   vBoard = [['','',''],['','',''],['','','']]
   turnPlayer = 'player1'
   document.querySelector('h2').innerHTML = 'Vez de: <span id="turnPlayer"></span>'
@@ -22,6 +26,35 @@ function initializeGame() {
     element.innerText = ''
     element.addEventListener('click', handleBoardClick)
   })
+}
+
+function getWinRegions() {
+  const winRegions = []
+  if (vBoard[0][0] && vBoard[0][0] === vBoard[0][1] && vBoard[0][0] === vBoard[0][2])
+    winRegions.push("0.0", "0.1", "0.2")
+  if (vBoard[1][0] && vBoard[1][0] === vBoard[1][1] && vBoard[1][0] === vBoard[1][2])
+    winRegions.push("1.0", "1.1", "1.2")
+  if (vBoard[2][0] && vBoard[2][0] === vBoard[2][1] && vBoard[2][0] === vBoard[2][2])
+    winRegions.push("2.0", "2.1", "2.2")
+  if (vBoard[0][0] && vBoard[0][0] === vBoard[1][0] && vBoard[0][0] === vBoard[2][0])
+    winRegions.push("0.0", "1.0", "2.0")
+  if (vBoard[0][1] && vBoard[0][1] === vBoard[1][1] && vBoard[0][1] === vBoard[2][1])
+    winRegions.push("0.1", "1.1", "2.1")
+  if (vBoard[0][2] && vBoard[0][2] === vBoard[1][2] && vBoard[0][2] === vBoard[2][2])
+    winRegions.push("0.2", "1.2", "2.2")
+  if (vBoard[0][0] && vBoard[0][0] === vBoard[1][1] && vBoard[0][0] === vBoard[2][2])
+    winRegions.push("0.0", "1.1", "2.2")
+  if (vBoard[0][2] && vBoard[0][2] === vBoard[1][1] && vBoard[0][2] === vBoard[2][0])
+    winRegions.push("0.2", "1.1", "2.0")
+  return winRegions
+}
+
+function handleWin(regions){
+  regions.forEach(function (region){
+    document.querySelector('[data-region="' + region + '"]').classList.add('win')
+  })
+  const playerName = document.getElementById(turnPlayer).value
+  document.querySelector('h2').innerHTML = playerName + " Venceu!!"
 }
 
 function handleBoardClick(ev){
@@ -41,6 +74,19 @@ function handleBoardClick(ev){
   console.clear()
   console.table(vBoard)
   disableRegion(span)
+  const winRegions = getWinRegions()
+  if(winRegions.length > 0) {
+    handleWin(winRegions)
+    document.querySelectorAll('.cursor-pointer').forEach(function (region){
+      disableRegion(region)
+    })
+  }else if(vBoard.flat().includes('')){
+    turnPlayer = turnPlayer === 'player1' ? 'player2' : 'player1'
+    updateTitle()
+  }
+  else{
+    document.querySelector('h2').innerHTML = 'EMPATE!'
+  }
 }
 
 document.getElementById('start').addEventListener('click', initializeGame)
